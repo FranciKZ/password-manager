8b1bb01678625f9d2bb0c6ef4ba20dd4bc1f70f6
francikz <francikz@miamioh.edu>
2018-08-07 14:44:04 -0400

    Working on basic file structure

---
+++ main.py
@@ -0,0 +1,13 @@
+from databaseFunction import *
+
+def genPassword():
+    password = "This is a password"
+    return password
+
+def main():
+    # how to use databaseFunction functions
+    # password = RetrieveData.unencrypt('test')
+    print(password)
+
+if __name__ == '__main__':
+    main()
